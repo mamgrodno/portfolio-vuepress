4e4fc0ceb29bf5fc5d6186fe248f745ddc80badc
Kitasio <56487191+Kitasio@users.noreply.github.com>
2020-11-04 16:36:21 +0200

    added the slider back

--- portfolio/.vuepress/components/WorksList.vue
+++ portfolio/.vuepress/components/WorksList.vue
@@ -6,18 +6,17 @@
 
 
 
-    <!-- <carousel :autoplay="true"
+    <carousel :autoplay="true"
               :autoplayTimeout="2000"
               :loop="true"
               :perPage="1">
 
       <slide v-for="post in posts"
-             :key="post.title"> -->
+             :key="post.title">
         <router-link
           :to="post.path"
           tag="div"
-          v-for="post in posts"
-          :key="post.title"
+          
           class="post"
           :style="{ backgroundImage: `url(${post.frontmatter.thumbnail})` }"
         >
@@ -27,22 +26,22 @@
             <span v-if="post.frontmatter.description">{{ post.frontmatter.description }}</span>
           </div>
         </router-link>
-      <!-- </slide> -->
+      </slide>
       
-    <!-- </carousel> -->
+    </carousel>
 
       </div>
 
 </template>
 
 <script>
-// import { Carousel, Slide } from 'vue-carousel';
+import { Carousel, Slide } from 'vue-carousel';
 
   export default {
-    // components: {
-    //   Carousel,
-    //   Slide
-    // },
+    components: {
+      Carousel,
+      Slide
+    },
     computed: {
       posts() {
         return this.$site.pages
