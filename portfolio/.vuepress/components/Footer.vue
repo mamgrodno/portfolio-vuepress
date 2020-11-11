f1a7aa99543786f466dd1c3c52e2d50e36627b56
Kitasio <56487191+Kitasio@users.noreply.github.com>
2020-11-11 17:01:40 +0200

    footer

--- portfolio/.vuepress/components/Footer.vue
+++ portfolio/.vuepress/components/Footer.vue
@@ -1,12 +1,32 @@
 <template>
-  <span class="text">{{ $site.themeConfig.footer }}</span>
+  <div class="container">
+    <div class="box">
+      <div>
+        МАМ — Modern Art Museum, Гродно. Площадка для современного искусства, проекты в свободном формате, без цензуры, на различных плоскостях и поверхностях, актуальная информация о событиях в арт-пространстве Гродно. Клуб MAM — сообщество, готовое поддержать и развивать современное искусство, стать участниками, спонсорами арт-инициатив.
+      </div>
+      <div>
+        Контакты: mamgrodno@gmail.com
+      </div>
+    </div>
+    <span class="text">{{ $site.themeConfig.footer }}</span>
+  </div>
 </template>
 
 <style scoped>
+  .container {
+    background-color: #f5f5f5;
+    margin-top: 10rem;
+  }
   .text {
     display: block;
     font-size: 0.8rem;
     text-align: left;
-    padding: 4rem 5vw;
+    padding: 2rem 5vw;
+  }
+  .box {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+    justify-items: center;
+    padding: 2rem 3rem;
   }
 </style>
--- portfolio/index.md
+++ portfolio/index.md
@@ -10,8 +10,7 @@ project3_text: 'Проекты или вставить свой текст <span
   <strong>жирный </strong>'
 project4_text: 'Проекты или вставить свой текст <span style="text-decoration: underline;">подчеркиваю</span>
   <strong>жирный </strong>'
-footer_text: '<hr style="margin-bottom:2rem;">МАМ&nbsp;&mdash; Modern Art Museum, Гродно. Площадка для современного искусства, проекты в&nbsp;свободном формате, без цензуры, на&nbsp;различных плоскостях и&nbsp;поверхностях, актуальная информация о&nbsp;событиях в&nbsp;арт-пространстве Гродно.
-Клуб MAM&nbsp;&mdash; сообщество, готовое поддержать и&nbsp;развивать современное искусство, стать участниками, спонсорами арт-инициатив.<br>Контакты: mamgrodno@gmail.com'
+
 
 ---
 <transition name="router-anim" enter-active-class="animate__animated animate__fadeIn" leave-active-class="animate__animated animate__fadeIn">
