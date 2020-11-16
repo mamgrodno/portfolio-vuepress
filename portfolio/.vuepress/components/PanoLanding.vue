2dcd0482ede9bad1badf8bea39d2bbb8176e554e
Kitasio <56487191+Kitasio@users.noreply.github.com>
2020-11-16 13:54:00 +0200

    fix design in panoramas

--- portfolio/.vuepress/components/PanoLanding.vue
+++ portfolio/.vuepress/components/PanoLanding.vue
@@ -39,8 +39,8 @@
   .pano img{
     height: 70vh;
     width: 100vw;
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-    border-radius: 10px;
+    /* box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    border-radius: 10px; */
     margin-bottom: 3rem;
     object-fit: cover;
     transition: all .2s ease-in-out;
