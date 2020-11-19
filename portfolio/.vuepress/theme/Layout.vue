c1f650cd1fd8ef8f50278fd592aa6582863cfac7
Kitasio <56487191+Kitasio@users.noreply.github.com>
2020-11-19 14:43:20 +0200

    more font options and tags at top

--- portfolio/.vuepress/theme/Layout.vue
+++ portfolio/.vuepress/theme/Layout.vue
@@ -18,11 +18,11 @@
       <div v-if="isSingleProject">
 
         <transition name="router-anim" enter-active-class="animate__animated animate__fadeIn" leave-active-class="animate__animated animate__fadeIn">
-        <!-- <SingleProjectHeader
+        <SingleProjectHeader
           :title="$page.frontmatter.heading"
           :year="$page.frontmatter.year.toString()"
           :categories="$page.frontmatter.categories"
-        /> -->
+        />
         </transition>
         <transition name="router-anim" enter-active-class="animate__animated animate__fadeIn" leave-active-class="animate__animated animate__fadeIn">
         <Content/>
@@ -32,11 +32,11 @@
       <!-- Single little project -->
       <div v-if="isSingleProj">
         <transition name="router-anim" enter-active-class="animate__animated animate__fadeIn" leave-active-class="animate__animated animate__fadeIn">
-        <!-- <SingleProjectHeader
+        <SingleProjectHeader
           :title="$page.frontmatter.heading"
           :year="$page.frontmatter.year.toString()"
           :categories="$page.frontmatter.categories"
-        /> -->
+        />
         </transition>
         <transition name="router-anim" enter-active-class="animate__animated animate__fadeIn" leave-active-class="animate__animated animate__fadeIn">
         <Content/>
@@ -133,7 +133,7 @@
 <style>
   @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap');
   @import "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css";
-  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 
   :root {
     --color-black: #1c1c1c;
