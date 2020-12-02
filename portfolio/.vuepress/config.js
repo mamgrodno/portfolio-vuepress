0bd0155412eabeb2aceebf10902d0537f6577f7e
Kitasio <56487191+Kitasio@users.noreply.github.com>
2020-12-02 15:41:40 +0200

    ym in the head

--- portfolio/.vuepress/config.js
+++ portfolio/.vuepress/config.js
@@ -38,7 +38,8 @@ module.exports = {
     filters: config.filters
   },
   head: [
-    ['link', { rel: "icon", href: config.favicon }]
+    ['link', { rel: "icon", href: config.favicon }],
+    ['script', {type: "text/javascript"}, '(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(70005481, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true});']
   ],
   markdown: {
     anchor: {
