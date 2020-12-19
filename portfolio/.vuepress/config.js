1874a102279af07b414fd9bc12715deab73d5f61
Kitasio <56487191+Kitasio@users.noreply.github.com>
2020-12-19 21:22:06 +0200

    rel logo

--- portfolio/.vuepress/config.js
+++ portfolio/.vuepress/config.js
@@ -46,7 +46,7 @@ module.exports = {
     ['meta', { name: "keywords", content: config.keywords }],
     ['meta', { name: "description", content: config.description }],
     ['link', { rel: "icon", href: config.favicon }],
-    ['link', { rel: "logo", href: config.logo }],
+    ['link', { rel: "image_src", href: config.logo }],
     ['script', {type: "text/javascript"}, '(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(70005481, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true});']
   ],
   markdown: {
