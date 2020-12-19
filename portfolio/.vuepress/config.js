6b4e6036346493edaa447fe95769d4ce6a2f7fff
Kitasio <56487191+Kitasio@users.noreply.github.com>
2020-12-19 21:03:32 +0200

    adadada

--- portfolio/.vuepress/config.js
+++ portfolio/.vuepress/config.js
@@ -44,6 +44,7 @@ module.exports = {
     ['meta', { property: "og:image", content: 'https://www.mamgrodno.com'+config.logo }],
     ['meta', { property: "og:url", content: "www.mamgrodno.com" }],
     ['meta', { name: "keywords", content: config.keywords }],
+    ['meta', { name: "description", content: config.description }],
     ['link', { rel: "icon", href: config.favicon }],
     ['script', {type: "text/javascript"}, '(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(70005481, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true});']
   ],
