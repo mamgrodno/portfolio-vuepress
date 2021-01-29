99cd9c12527987d6cd3ee4e3f2c99d9e65c9b32b
Kitasio <56487191+Kitasio@users.noreply.github.com>
2021-01-29 16:03:42 +0200

    daadssaddsa

--- portfolio/.vuepress/config.js
+++ portfolio/.vuepress/config.js
@@ -24,13 +24,13 @@ module.exports = {
     footer_text: config.footer_text,
     filters: config.filters,
   },
-  // head: [
-  //   ['meta', { name: "keywords", content: config.keywords }],
-  //   ['meta', { name: "description", content: config.description }],
-  //   ['link', { rel: "icon", href: config.favicon }],
-  //   ['link', { rel: "image_src", href: config.logo }],
-  //   ['script', {type: "text/javascript"}, '(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(70005481, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true});']
-  // ],
+  head: [
+    ['meta', { name: "keywords", content: config.keywords }],
+    ['meta', { name: "description", content: config.description }],
+    ['link', { rel: "icon", href: config.favicon }],
+    ['link', { rel: "image_src", href: config.logo }],
+    ['script', {type: "text/javascript"}, '(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(70005481, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true});']
+  ],
   markdown: {
     anchor: {
       permalink: false
