6bb016a0b7c80fc2ae1c471e9b334c351a7b58cc
Kitasio <56487191+Kitasio@users.noreply.github.com>
2021-02-27 12:18:31 +0200

    global img max height 100

--- portfolio/.vuepress/theme/Layout.vue
+++ portfolio/.vuepress/theme/Layout.vue
@@ -171,6 +171,10 @@
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   } */
 
+  img {
+    max-height: 100vh;
+  }
+
   .agile__actions button {
     background: none;
     color: inherit;
