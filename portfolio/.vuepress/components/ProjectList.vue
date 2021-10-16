1de88fc58a0ee4c4a6ac4692d5b76c46eecd8b1c
Kitasio <56487191+Kitasio@users.noreply.github.com>
2021-10-16 21:00:51 +0300

    animations and boldakov_trinity

--- portfolio/.vuepress/components/ProjectList.vue
+++ portfolio/.vuepress/components/ProjectList.vue
@@ -51,9 +51,9 @@
       ScrollTrigger.batch(".projectbox", {
         interval: .2,
         onEnter: batch => gsap.to(batch, {autoAlpha: 1, stagger: 0.1, opacity: 1, y: 0, ease: "power3"}),
-        onLeave: batch => gsap.set(batch, {opacity: 0, y: -50}),
-        onEnterBack: batch => gsap.to(batch, {autoAlpha: 1, stagger: 0.1, opacity: 1, y: 0, ease: "power3"}),
-        onLeaveBack: batch => gsap.set(batch, {opacity: 0, y: 50}),
+        // onLeave: batch => gsap.set(batch, {opacity: 0, y: -50}),
+        // onEnterBack: batch => gsap.to(batch, {autoAlpha: 1, stagger: 0.1, opacity: 1, y: 0, ease: "power3"}),
+        // onLeaveBack: batch => gsap.set(batch, {opacity: 0, y: 50}),
       })
     },
     data() {
---
+++ portfolio/.vuepress/dist/assets/js/12.0b12f71d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{333:function(t,n,s){},352:function(t,n,s){"use strict";s(333)},381:function(t,n,s){"use strict";s.r(n);var e={},a=(s(352),s(45)),c=Object(a.a)(e,(function(){var t=this.$createElement;return(this._self._c||t)("span",{staticClass:"label"},[this._t("default")],2)}),[],!1,null,"3b34421d",null);n.default=c.exports}}]);
